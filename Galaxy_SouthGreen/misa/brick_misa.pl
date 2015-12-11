d892398231d1cf977f5881b025cc276035001a77
MarilyneSummo <marilyne.summo@cirad.fr>
2015-12-11 13:29:02 +0100

    suppression de lien en dur dans misa, remplacé par une variable du repertoire de l'outil

--- Galaxy_SouthGreen/misa/brick_misa.pl
+++ Galaxy_SouthGreen/misa/brick_misa.pl
@@ -9,16 +9,18 @@ my $file=shift;
 my $result_file=shift;
 my $stat_file=shift;
 
+my $tool_dir = shift;
+
 #print "\tStart Misa\n";
-system ("/home/galaxydev/galaxy/tools/SouthGreen/misa/misa.pl $file $stat_file ");
+system ("$tool_dir/misa.pl $file $stat_file ");
 
 #print "\tReport Misa\n";
-system ("/home/galaxydev/galaxy/tools/SouthGreen/misa/p3_in.pl $file.misa ");
+system ("$tool_dir/p3_in.pl $file.misa ");
 
 
 #print "\tStart Primer3\n";
 system ("primer3_core < $file.p3in > $file.p3out ");
 
 #print "\tReport Primer3\n";
-system ("/home/galaxydev/galaxy/tools/SouthGreen/misa/p3_out.pl $file.p3out $file.misa $result_file ");
+system ("$tool_dir/p3_out.pl $file.p3out $file.misa $result_file ");
 
