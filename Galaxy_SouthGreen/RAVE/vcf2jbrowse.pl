adaa4ea5af3f346c8cfbd60d613e8ea182119398
MarilyneSummo <marilyne.summo@cirad.fr>
2016-09-28 10:09:13 +0200

    Jbrowse URL

--- Galaxy_SouthGreen/RAVE/vcf2jbrowse.pl
+++ Galaxy_SouthGreen/RAVE/vcf2jbrowse.pl
@@ -20,6 +20,7 @@ my $html;
 my $host = "salanque.cirad.fr";
 my $user = "galaxy";
 my $json = "template.json";
+
 my $scp = Net::SCP->new($host);
 $scp->login($user); 
 GetOptions(
@@ -30,9 +31,15 @@ GetOptions(
 ) ;
 my $file_vcf = $tool_directory ."/galaxy$$.vcf";
 #my $tabix_cmd = "source " . $tool_directory. "/module_vcf2jbrowse.sh; bgzip ". $vcffile ."; tabix -p vcf ". $vcffile .".gz";
+
+
 my $tabix_cmd = "source " . $tool_directory. "/module_vcf2jbrowse.sh; cp ". $vcffile ." " . $file_vcf ."; bgzip ". $file_vcf ." ; tabix -p vcf ". $file_vcf .".gz";
 system("scp ". $user ."@". $host.":/opt/projects/jbrowse.southgreen.fr/prod/jbrowse/oryza_sativa_japonica_v7/trackList.json " . $tool_directory."/trackList.json");
-
+my $output = `grep -v "#" $vcffile | sed -n '1p' | cut -f 1,2 `;
+chomp($output);
+my ($chr,$start) = (split(/\t/,$output))[0,1];
+my $end = $start + 500000;
+my $location = $chr."%3A".$start."..".$end; 
 my $json;
 {
     local $/; #Enable 'slurp' mode
@@ -56,9 +63,9 @@ system("scp ". $file_vcf.".gz.tbi ". $user ."@". $host.":/opt/projects/jbrowse.s
 system("scp ". $file_vcf.".gz ". $user ."@". $host.":/opt/projects/jbrowse.southgreen.fr/prod/jbrowse/oryza_sativa_japonica_v7/tmp/". $file_gz);
 system("scp ".$tool_directory."/galaxy$$.json ". $user ."@". $host.":/opt/projects/jbrowse.southgreen.fr/prod/jbrowse/oryza_sativa_japonica_v7/");
 system("scp ".$tool_directory."/trackList.json ". $user ."@". $host.":/opt/projects/jbrowse.southgreen.fr/prod/jbrowse/oryza_sativa_japonica_v7/");
- 
+system("rm  ".$tool_directory."/trackList.json ".$tool_directory."/galaxy$$.json ".$file_vcf.".gz ".$file_vcf.".gz.tbi ");
 
 open(HTML,">$html");
-print HTML "<html><body><div><a href='http://jbrowse.southgreen.fr/?data=oryza_sativa_japonica_v7&loc=chr03%3A16000000..16500000&tracks=DNA%2CMSUGeneModels%2Ctmp%2fgalaxy$$.vcf.gz&highlight=' target='_blank'>View on Jbrowse</a></div></body></html>";
+print HTML "<html><body><div><a href='http://jbrowse.southgreen.fr/?data=oryza_sativa_japonica_v7&".$location."&tracks=DNA%2CMSUGeneModels%2Ctmp%2fgalaxy$$.vcf.gz&highlight=' target='_blank'>View on Jbrowse</a></div></body></html>";
 close HTML;
 
