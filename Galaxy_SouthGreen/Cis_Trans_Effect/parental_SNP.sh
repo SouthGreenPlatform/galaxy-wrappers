8e3043e6c7d2fa1e5de1d1520762ecf7bf99ce9c
MarilyneSummo <marilyne.summo@cirad.fr>
2016-09-30 09:35:31 +0200

    improve Cis_Trans_Effect

--- Galaxy_SouthGreen/Cis_Trans_Effect/parental_SNP.sh
+++ Galaxy_SouthGreen/Cis_Trans_Effect/parental_SNP.sh
@@ -10,11 +10,14 @@ nameparent1=$4
 nameparent2=$5
 nb_indv_parent1=$6
 nb_indv_parent2=$7
-
+miss_parent1=$8
+miss_parent2=$9
+num_occ=${10}
+min_depth=${11}
 
 #echo $tool_path/parental_SNP.pl -i $filein -o $fileout -s1 $nameparent1 -s2 $nameparent2 -n1 $nb_indv_parent1 -n2 $nb_indv_parent2 >>$filelog
 
-perl $tool_path/parental_SNP.pl -i $filein -o $fileout -s1 $nameparent1 -s2 $nameparent2 -n1 $nb_indv_parent1 -n2 $nb_indv_parent2 >>$filelog 2>&1 
+perl $tool_path/parental_SNP.pl -i $filein -o $fileout -s1 $nameparent1 -s2 $nameparent2 -n1 $nb_indv_parent1 -n2 $nb_indv_parent2 -m1 $miss_parent1 -m2 $miss_parent2 -g $num_occ -p $min_depth >>$filelog 2>&1 
 
 
 
