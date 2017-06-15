db75fea933b201801a5c35d221a3c04c258bcb44
galaxydev <galaxydev@cc2-web1.(none)>
2017-06-15 14:59:23 +0200

    update circosjs

--- Galaxy_SouthGreen/circosjs/circosjs.pl
+++ Galaxy_SouthGreen/circosjs/circosjs.pl
@@ -5,56 +5,87 @@ use CGI;
 use Cwd;
 my $dir = getcwd;
 
-my $chromosome;
+my $chromosome; #Partie qui récupère les variables
 my $output;
 my @tracks;
 my @select;
 
-GetOptions ("chromosome=s" => \$chromosome,    # numeric
-              "tracks=s"   => \@tracks,      # string
-              "select=s"  => \@select,
-              "output=s" => \$output)   # flag
+GetOptions ("chromosome=s" => \$chromosome,    #récupère le fichier de chromosomes
+              "tracks=s"   => \@tracks,        #Récupère le tableau de fichier
+              "select=s"  => \@select,		   #Récupère le tableau de Select
+              "output=s" => \$output)          #Sortie du fichier
 or die("Error in command line arguments\n");
 
 open(O,">$output");
-my $q = CGI->new;        
-
+my $q = CGI->new;       
 print O $q->header('text/html'); # create the HTTP header
 print O $q->start_html(''); # start the HTML
-system("cp $chromosome \$HOME/galaxy/static/style/blue/ideogram/");
-my @part = split("/",$chromosome);
+
 
 my $base_url = "http://galaxy.southgreen.fr/galaxy/";
 if ($dir =~/galaxy_dev/){
-	$base_url = "http://cc2-web1.cirad.fr/galaxydev/";
+        $base_url = "http://cc2-web1.cirad.fr/galaxydev/";
+}
+
+my $chromosomelength;
+if ($chromosome && $chromosome ne 'None'){
+	system("cp $chromosome \$HOME/galaxy/static/style/blue/ideogram/");
+	my @part = split("/",$chromosome);
+	#Déplacement des fichiers dans un répértoire accessible depuis l'exterieur
+	$chromosomelength="$base_url/static/style/ideogram/".$part[$#part];
+}
+# guess chrom length
+else{
+	my $home = `echo \$HOME`;
+	$home =~s/\r//g;$home =~s/\n//g;
+	my $chromfile = int(rand(100000000));
+	my %size_max;
+	foreach my $i (0 .. $#tracks){
+		if ($select[$i] ne 'Chords'){
+			
+			open(T,$tracks[$i]);
+			while(<T>){
+				my ($chr,$pos) = split(/\s+/,$_);
+				if ($pos > $size_max{$chr}){
+					$size_max{$chr} = $pos;
+				}
+			}
+			close(T);
+		}
+	}
+	open(F,">$home/galaxy/static/style/blue/ideogram/$chromfile");
+	foreach my $chrom(sort {$a<=>$b} keys(%size_max)){
+		my $size = $size_max{$chrom};
+		print F "$chrom $size\n";
+	}
+	close(F);
+	$chromosomelength="$base_url/static/style/ideogram/$chromfile";
 }
 
 
-my $chromosomelength="$base_url/static/style/ideogram/".$part[$#part];
+#Création de la partie script hml qui va être envoyée a Galaxy pour l'interprétation
 my $iframe = qq~
-<form target="myIframe" action="http://cassava-genome.southgreen.fr/visu_genomeharvest/circosJS/demo/index.php" method="post" enctype="multipart/form-data">
+<form target="myIframe" action="http://genomeharvest.southgreen.fr/visu/circosJS/demo/index.php" method="post" enctype="multipart/form-data">
 <input type="hidden" name="chromosome" value="$chromosomelength" />
 ~;
 foreach my $i (0 .. $#tracks){
 	#print "$tracks[$i]";
 	system("cp $tracks[$i] \$HOME/galaxy/static/style/blue/ideogram/");
+
 	my @part = split("/",$tracks[$i]);
 	my $track_file = "$base_url/static/style/ideogram/".$part[$#part];
+
+	# replace tab by space if needed
+	my $sed_cmd = "sed -i \"s/\t/ /g\" \$HOME/galaxy/static/style/blue/ideogram/".$part[$#part];
+	system($sed_cmd);
 	$iframe .= qq~
 	<input type="hidden" name="select[]" id="annot" value="$select[$i]" />
-	<input type="hidden" name="data[]" id="annot" value="$track_file" />
-	~;
-}    
+	<input type="hidden" name="data[]" id="annot" value="$track_file" />~;
+}
 $iframe .= qq~
 <input type="submit" style="display: none;" id="load" value="reload">
-</form>
-
-<iframe src="" id="myIframe" name="myIframe" width=\"100%\" height=\"1700\" style='border:solid 0px black;'></iframe>
-<script>
-document.getElementById("load").click();
-</script>
+</form><iframe src="" id="myIframe" name="myIframe" width=\"100%\" height=\"1700\" style='border:solid 0px black;'></iframe>
+<script>document.getElementById("load").click();</script>
 ~;
-
 print O $iframe;
-
 close(O);
