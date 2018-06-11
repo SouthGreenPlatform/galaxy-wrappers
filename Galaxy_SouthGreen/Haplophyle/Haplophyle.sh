fc39555f6ed251a8f66bd644f9ccbdceeebd9a21
acomte <aurore.comte@ird.fr>
2018-06-11 11:39:43 +0200

    update sh

--- Galaxy_SouthGreen/Haplophyle/Haplophyle.sh
+++ Galaxy_SouthGreen/Haplophyle/Haplophyle.sh
@@ -4,14 +4,14 @@ tool_path=$(dirname $0)
 
 filein=$1
 filein2=$2
-groups=$3
-fileout=$4
-dotfile=$5
-cytoscape_html=$6
-logfile=$7
-
+fileout=$3
+dotfile=$4
+cytoscape_html=$5
+logfile=$6
+groups=$7
 
 perl $tool_path/Haplophyle.pl --input $filein --groups $groups --stats $filein2 --dot $dotfile --out $fileout --html $cytoscape_html >>$logfile 2>&1
+
 
 
 
