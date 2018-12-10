56cab3c974da56249357fce19154c7552fee70ed
acomte <aurore.comte@ird.fr>
2018-12-10 21:22:44 +0100

    add HighchartsScatter

---
+++ Galaxy_SouthGreen/HighchartsScatter/InteractiveScatter.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+input=$1
+output=$2
+x_axis=$3
+y_axis=$4
+z_axis=$5
+id=$6
+category=$7
+
+directory=`dirname $0`
+mkdir tmpdir$$
+
+
+perl $directory/InteractiveScatter.pl -i $input -o $output -x $x_axis -y $y_axis -n $id -c $category -z $z_axis
